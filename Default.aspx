90ff1d28f575e97dd58633e36c6a73685633e650
smithbrianscott <smithbrianscott23@gmail.com>
2017-10-19 13:08:52 -0400

    Gridview, functions for delete and add of files.

--- Default.aspx
+++ Default.aspx
@@ -40,8 +40,52 @@
     </div>--%>
 
     <div class="row">
-        <div class="col-md-12">
-
+        <div class="col-xs-11 col-lg-8">
+            <div class="form-group">
+                <%--Documents--%>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <label for="txtFolder">Folder Name</label>
+                <asp:TextBox runat="server" ID="txtFolder" Text="FolderName" CssClass="form-control"></asp:TextBox>
+                <asp:Button runat="server" ID="btnGetDocuments" Text="Get" CssClass="btn btn-primary" OnClick="btnGetDocuments_Click" /><br /><br />
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <label for="fileRCA">Attachments</label><br />
+            <asp:FileUpload ID="fileRCA" runat="server" AllowMultiple="true"  /> 
+            <asp:Button ID="UploadFile" runat="server" CausesValidation="false" Text="Upload" CssClass="btn btn-sm btn-primary margin-bottom-6" OnClick="UploadFile_Click" />
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <Label ID="lblUploadStatus" hidden="hidden" runat="server" style="color:darkred">Please wait while we upload your documents</Label>
+            <br />
+            <asp:GridView ID="grdFiles" CssClass="table" AutoGenerateColumns="false" runat="server">
+                <Columns>
+                    <asp:TemplateField HeaderText="Name">
+                        <ItemTemplate>
+                            <asp:HyperLink ID="lnkName" runat="server" NavigateUrl='<%# Eval("URL") %>' Target="_blank" Text='<%# Eval("Title") %>'></asp:HyperLink>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                    <asp:BoundField DataField="Created" HeaderText="Created" />
+                    <asp:BoundField DataField="Modified" HeaderText="Modified" />
+                    <asp:BoundField DataField="ID" HeaderText="ID" />
+                    <asp:TemplateField>
+                        <ItemTemplate>
+                            <asp:LinkButton ID="btnDeleteDocument" Text="Delete" runat="server" OnClick="btnDeleteDocument_Click" CausesValidation="false"></asp:LinkButton>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+                </Columns>
+                <EmptyDataTemplate>
+                    No Files
+                </EmptyDataTemplate>
+            </asp:GridView>
         </div>
     </div>
 
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using microsoft_graph_files_web_forms.Classes;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,17 +13,75 @@ namespace microsoft_graph_files_web_forms
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Auth auth = new Auth();
-            
-            if (auth.GetAppAccessToken())
+            string path = "/" + txtFolder.Text;
+            GetDocuments(path);
+        }
+
+        protected void UploadFile_Click(object sender, EventArgs e)
+        {
+            lblUploadStatus.Visible = true;
+            string path = "/" + txtFolder.Text;
+            if (UploadDocuments(path))
             {
-                // You success retreived an access token.
-                string token = auth.AppAccessToken;
+                // Success
+                lblUploadStatus.Visible = false;
+                GetDocuments(path);
             }
             else
             {
-                // Something didn't work.
+                // Something is wrong
             }
+        }
+
+        private bool UploadDocuments(string FolderPath)
+        {
+            bool success = false;
+            if (fileRCA.HasFile)
+            {
+                foreach (var file in fileRCA.PostedFiles)
+                {
+                    SharePoint sp = new SharePoint();
+                    success = sp.UploadDocument(file, FolderPath);
+                }
+            }
+            else
+            {
+                // There isn't any files in the upload control. 
+            }
+
+            return success;
+        }
+
+        private void GetDocuments(string FolderPath)
+        {
+            SharePoint sp = new SharePoint();
+            DataTable dt = sp.GetDocuments(FolderPath);
+
+            grdFiles.DataSource = dt;
+            grdFiles.DataBind();
+        }
+
+        protected void btnDeleteDocument_Click(object sender, EventArgs e)
+        {
+            GridViewRow row = (GridViewRow)((LinkButton)sender).NamingContainer;
+            string itemID = row.Cells[3].Text;
+
+            SharePoint sp = new SharePoint();
+            if (sp.DeleteDocument(itemID))
+            {
+                string path = "/" + txtFolder.Text;
+                GetDocuments(path);
+            }
+            else
+            {
+                //Something is wrong
+            }
+        }
+
+        protected void btnGetDocuments_Click(object sender, EventArgs e)
+        {
+            string path = "/" + txtFolder.Text;
+            GetDocuments(path);
         }
     }
 }
